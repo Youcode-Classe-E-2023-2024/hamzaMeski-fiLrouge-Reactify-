2f525c95e7f3283bd6ad3b93eaef8b25ac345492
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-27 18:23:16 +0100

    post route setup

--- main/app/Http/Controllers/PostController.php
+++ main/app/Http/Controllers/PostController.php
@@ -4,7 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Post;
+
 class PostController extends Controller
 {
-    //
+    public function show(Post $post) {
+
+    }
+
+    public function store() {
+
+    }
+
+    public function edit(Post $post) {
+
+    }
+
+    public function update(Post $post) {
+
+    }
+
+    public function destroy(Post $post) {
+
+    }
 }
--- main/routes/web.php
+++ main/routes/web.php
@@ -3,15 +3,29 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 
 /* main route */
 Route::get('/', [MainController::class, 'index'])->name('main');
 
 /* auth route */
 Route::get('/register', [AuthController::class, 'register'])->name('register');
+
 Route::post('/register', [AuthController::class, 'store'])->name('register.store');
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
+
 Route::post('/login', [AuthController::class, 'authenticate'])->name('login.authenticate');
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+/* post route */
+Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
+
+Route::post('/post/{post}', [PostController::class, 'store'])->name('post.store');
+
+Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+
+Route::put('/post/{post}', [PostController::class, 'update'])->name('post.update');
+
+Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
