e813b099f076bf412b2fea70c191b57ef72c1da5
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-03-24 01:28:51 +0100

    right side bar

--- resources/views/QA-page/main.blade.php
+++ resources/views/QA-page/main.blade.php
@@ -62,9 +62,9 @@
                 <li class="px-4 py-1">Explore all collectives</li>
             </ul>
         </div>
-        <main class=" h-full w-full overflow-auto">
+        <main class=" h-full w-full overflow-auto grid grid-cols-5">
             {{-- Questions Section start --}}
-                <div class="w-[60%] h-full border-r-[1px] border-solid border-gray-500 pt-6">
+                <div class="col-span-3 h-full pt-6">
                     <div class="flex items-center justify-between px-6">
                         <span class="text-2xl">All Questions</span>
                         <a href="" class="text-white bg-[#03a4ed] px-4 py-2 rounded-md text-[14px]">Ask Question</a>
@@ -81,8 +81,8 @@
                         </table>
 
                     </div>
-                    
-                    <div class="border-t-[1px] px-6 py-6">
+
+                    <div class="border-t-[1px] px-6 py-6 w-full">
                         <div class="grid grid-cols-5 gap-y-2 gap-x-4 border-solid border-gray-400">
                             <div class="col-span-1 flex justify-end text-[14px]"><span>1 votes</span></div>
                             <div class="col-span-4 text-[14px] flex text-sky-500"><a href="">How to create a zoom animation in Jetpack Compose</a></div>
@@ -122,13 +122,101 @@
                             <span class="text-[13px] text-gray-700">asked 1 min ago</span>
                         </div>
                     </div>
-
                 </div>
             {{-- Questions Section end --}}
 
             {{-- blogs Sections, top hot questions & related tags Section start --}}
-                <div class="w-[40%] h-full">
+                <div class="col-span-2 h-full mt-6 mr-20">
+                    {{-- blogs start --}}
+                        <div class=" border-[1px] border-[#fe3f40] border-solid text-[14px]">
+                            <p class="border-b-[1px] border-[#fe3f40] border-solid font-bold p-2 bg-[#fe3f40] text-white">The reactify blog</p>
+                            <ul class="px-4 py-2">
+                                <li>
+                                    <a href="" class="flex items-center gap-1">
+                                        <ion-icon name="create"></ion-icon>
+                                        <span>Why the creator of Node.js® created a new JavaScript runtime</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="flex items-center gap-1">
+                                        <ion-icon name="create"></ion-icon>
+                                        <span>Is AI making your code worse?</span>
+                                    </a>
+                                </li>
+                            </ul>
+                            <p class="border-b-[1px] border-[#fe3f40] border-solid font-bold p-2 bg-[#fe3f40] text-white">Featured</p>
+                            <ul class="px-4 py-2">
+                                <li>
+                                    <a href="" class="flex items-center gap-1">
+                                        <ion-icon name="create"></ion-icon>
+                                        <span>Why the creator of Node.js® created a new JavaScript runtime</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="flex items-center gap-1">
+                                        <ion-icon name="create"></ion-icon>
+                                        <span>Why the creator of Node.js® created a new JavaScript runtime</span>
+                                    </a>
+                                </li>
+                            </ul>
+                            <p class="border-b-[1px] border-[#fe3f40] border-solid font-bold p-2 bg-[#fe3f40] text-white">Hot Meta Posts</p>
+                            <ul class="px-4 py-2">
+                                <li>
+                                    <a href="" class="flex items-center gap-1">
+                                        <ion-icon name="create"></ion-icon>
+                                        <span>Why the creator of Node.js® created a new JavaScript runtime</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="flex items-center gap-1">
+                                        <ion-icon name="create"></ion-icon>
+                                        <span>Why the creator of Node.js® created a new JavaScript runtime</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    {{-- blogs end --}}
 
+                    {{-- related tags start --}}
+                        <div>
+                            <h1 class="text-2xl mt-10">Related Tags</h1>
+                            <ul class="flex flex-col justify-center gap-2">
+                                <li class="flex gap-1">
+                                    <a href="" class="text-[13px] text-blue-600 bg-blue-200 py-1 px-2 rounded-md mr-1">android</a>
+                                    <span class="text-[12px]">x 45222</span>
+                                </li>
+                                <li class="flex gap-1">
+                                    <a href="" class="text-[13px] text-blue-600 bg-blue-200 py-1 px-2 rounded-md mr-1">kotlin</a>
+                                    <span class="text-[12px]">x 45222</span>
+                                </li>
+                                <li class="flex gap-1">
+                                    <a href="" class="text-[13px] text-blue-600 bg-blue-200 py-1 px-2 rounded-md mr-1">swift</a>
+                                    <span class="text-[12px]">x 45222</span>
+                                </li>
+                                <li class="flex gap-1">
+                                    <a href="" class="text-[13px] text-blue-600 bg-blue-200 py-1 px-2 rounded-md mr-1">user-interface</a>
+                                    <span class="text-[12px]">x 4222</span>
+                                </li>
+                            </ul>
+                        </div>
+                    {{-- related tags end --}}
+
+                    {{-- hot questions start --}}
+                    <div>
+                        <h1 class="text-2xl mt-10">Top Hot Questions</h1>
+                        <ul class="flex flex-col justify-center gap-2">
+                            <li class="flex gap-1">
+                                <a href="" class="text-[14px] text-blue-500">The word *X* means Y: Should Y be in italics, quotes, or neither?</a>
+                            </li>
+                            <li class="flex gap-1">
+                                <a href="" class="text-[14px] text-blue-500">The word *X* means Y: Should Y be in italics, quotes, or neither?</a>
+                            </li>
+                            <li class="flex gap-1">
+                                <a href="" class="text-[14px] text-blue-500">The word *X* means Y: Should Y be in italics, quotes, or neither?</a>
+                            </li>
+                        </ul>
+                    </div>
+                    {{-- hot questions end --}}
                 </div>
             {{-- blogs Sections, top hot questions & related tags Section end --}}
         </main>
